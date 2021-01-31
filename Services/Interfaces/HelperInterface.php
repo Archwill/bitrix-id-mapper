b593f04f7086f331427a0662882e0bcacefebe25
Konstantin Zharkov <archwill93@gmail.com>
2021-01-31 20:57:49 +0700

    Update HelperInterface.php

--- Services/Interfaces/HelperInterface.php
+++ Services/Interfaces/HelperInterface.php
@@ -6,12 +6,7 @@ namespace Services\Interfaces;
 
 interface HelperInterface
 {
-    public function setFilter(Array $filter);
-
-    public function setIblock($iblockId);
-
-    public function setSelect(Array $select);
-
+    
     public function getEntityById($iblockId, $id, Array $select);
 
     public function getEntityByCode($iblockId, $code, Array $select);
